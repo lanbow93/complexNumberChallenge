f9d677a57fb46c9c1ea567423c9b8d07af6e1c6c
lanbow93 <the.doctor1501@gmail.com>
2024-09-06 13:53:13 -0500

    tests added back

--- src/App.java
+++ src/App.java
@@ -2,15 +2,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         ComplexNumber one = new ComplexNumber(1.0, 1.0);
         ComplexNumber number = new ComplexNumber(2.5, -1.5);
-        System.out.println(one.getReal());
-    //     one.add(1,1);
-    //     System.out.println("one.real= " + one.getReal());
-    //     System.out.println("one.imaginary= " + one.getImaginary());
-    //     one.subtract(number);
-    //     System.out.println("one.real= " + one.getReal());
-    //     System.out.println("one.imaginary= " + one.getImaginary());
-    //     number.subtract(one);
-    //     System.out.println("number.real= " + number.getReal());
-    //     System.out.println("number.imaginary= " + number.getImaginary());
+        one.add(1,1);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        one.subtract(number);
+        System.out.println("one.real= " + one.getReal());
+        System.out.println("one.imaginary= " + one.getImaginary());
+        number.subtract(one);
+        System.out.println("number.real= " + number.getReal());
+        System.out.println("number.imaginary= " + number.getImaginary());
     }
 }
